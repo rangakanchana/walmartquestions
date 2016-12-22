8db84246050e3f9068e74aeccd719326eec8b07a
rangakanchana <rangakanchana@gmail.com>
2016-12-22 08:01:40 -0800

    added few more classes

---
+++ com.walmart/src/ebay/SmallestNo.java
@@ -0,0 +1,46 @@
+package Arrays;
+public class SmallestNo {
+	/*public static void main(String[] args) {
+		int []arr = {23,15,2,16,89,76};
+		int num = arr[0];
+		for(int i =1;i<arr.length;i++){
+			if(num>arr[i]){
+				num = arr[i];
+			}
+		}
+		System.out.println("the smallest number in a given array is :"+num);
+}*/
+	public static int getSmallestNo(int arr[]){
+		
+		int smallNo = arr[0];
+		
+		for(int i=0;i<arr.length;i++){
+			
+			if(smallNo>arr[i]){
+				
+		smallNo = arr[i];	}
+		}
+		
+		return smallNo;
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		int arr[]={1,3,-1,0,97,34};
+		
+		int smallNo=SmallestNo.getSmallestNo(arr);
+		
+		System.out.println("the smallestno is:"+smallNo);
+		
+	}
+	}	
+	
+	
+	
+
+
+	
+
+
---
+++ com.walmart/src/ebay/StringRevWal.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.Strings;
+
+/**
+ * @author sridhar//we need to reverse the string such that walmart = lawmtra;
+ * 
+ *
+ */
+public class StringRevWal {
+	
+	public static String revString(String inputString){
+		
+		String result1 = "";
+		String result2 = "";
+		String result3 = "";
+		
+		int mid = inputString.length()/2;
+		
+		for(int i=(mid-1);i>=0;i--){
+			result1 = result1+inputString.charAt(i);
+		}
+		result2 = result2+inputString.charAt(mid);
+		
+		for(int i=inputString.length()-1;i>mid;i--){
+			
+			result3 = result3+inputString.charAt(i);
+		}
+		String result = result1+result2+result3;
+		
+		
+		System.out.println(result);
+		
+		return result;
+	}
+	    public static void main(String[] args) {
+	    	
+	    	StringRevWal.revString("WALMART");	
+		    
+	}
+	
+
+}
